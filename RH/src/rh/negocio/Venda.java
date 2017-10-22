d121c391588212ef1d103b037ef563afc0c2a877
Giovani Paganini <giovanipaganini@outlook.com>
2017-10-22 17:47:01 -0200

    Refatoração de código (identação) #3

--- RH/src/rh/negocio/Venda.java
+++ RH/src/rh/negocio/Venda.java
@@ -3,87 +3,86 @@ package rh.negocio;
 import java.util.Date;
 
 public class Venda {
-	
-	private int pk;
-	private int fkCliente;
-	private int fkVendedor;
-	private int numero;
-	private Date data;
-	
-	public Venda(int pk, int fkCliente, int fkVendedor, int numero, Date data) {
-		super();
-		this.pk = pk;
-		this.fkCliente = fkCliente;
-		this.fkVendedor = fkVendedor;
-		this.numero = numero;
-		this.data = data;
-	}
+    private int pk;
+    private int fkCliente;
+    private int fkVendedor;
+    private int numero;
+    private Date data;
 
-	public Venda(int fkCliente, int fkVendedor, int numero, Date data) {
-		super();
-		this.fkCliente = fkCliente;
-		this.fkVendedor = fkVendedor;
-		this.numero = numero;
-		this.data = data;
-	}
+    public Venda(int pk, int fkCliente, int fkVendedor, int numero, Date data) {
+            super();
+            this.pk = pk;
+            this.fkCliente = fkCliente;
+            this.fkVendedor = fkVendedor;
+            this.numero = numero;
+            this.data = data;
+    }
 
-	public Venda(int fkVendedor, int numero, Date data) {
-		super();
-		this.fkVendedor = fkVendedor;
-		this.numero = numero;
-		this.data = data;
-	}
+    public Venda(int fkCliente, int fkVendedor, int numero, Date data) {
+            super();
+            this.fkCliente = fkCliente;
+            this.fkVendedor = fkVendedor;
+            this.numero = numero;
+            this.data = data;
+    }
 
-	public Venda(int numero, Date data) {
-		super();
-		this.numero = numero;
-		this.data = data;
-	}
+    public Venda(int fkVendedor, int numero, Date data) {
+            super();
+            this.fkVendedor = fkVendedor;
+            this.numero = numero;
+            this.data = data;
+    }
 
-	public int getPk() {
-		return pk;
-	}
+    public Venda(int numero, Date data) {
+            super();
+            this.numero = numero;
+            this.data = data;
+    }
 
-	public void setPk(int pk) {
-		this.pk = pk;
-	}
+    public int getPk() {
+            return pk;
+    }
 
-	public int getFkCliente() {
-		return fkCliente;
-	}
+    public void setPk(int pk) {
+            this.pk = pk;
+    }
 
-	public void setFkCliente(int fkCliente) {
-		this.fkCliente = fkCliente;
-	}
+    public int getFkCliente() {
+            return fkCliente;
+    }
 
-	public int getFkVendedor() {
-		return fkVendedor;
-	}
+    public void setFkCliente(int fkCliente) {
+            this.fkCliente = fkCliente;
+    }
 
-	public void setFkVendedor(int fkVendedor) {
-		this.fkVendedor = fkVendedor;
-	}
+    public int getFkVendedor() {
+            return fkVendedor;
+    }
 
-	public int getNumero() {
-		return numero;
-	}
+    public void setFkVendedor(int fkVendedor) {
+            this.fkVendedor = fkVendedor;
+    }
 
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
+    public int getNumero() {
+            return numero;
+    }
 
-	public Date getData() {
-		return data;
-	}	
+    public void setNumero(int numero) {
+            this.numero = numero;
+    }
 
-	public void setData(Date data) {
-		this.data = data;
-	}
-	
-	@Override
-	public String toString() {
-		return "Venda{pk=" + pk + ", fkCliente=" + fkCliente + ", fkVendedor=" + fkVendedor + ", numero=" + numero
-				+ ", data=" + data + "}";
-	}	
+    public Date getData() {
+            return data;
+    }	
+
+    public void setData(Date data) {
+            this.data = data;
+    }
+
+    @Override
+    public String toString() {
+            return "Venda{pk=" + pk + ", fkCliente=" + fkCliente + ", fkVendedor=" + fkVendedor + ", numero=" + numero
+                            + ", data=" + data + "}";
+    }	
 
 }
