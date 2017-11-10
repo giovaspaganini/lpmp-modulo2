db9b15fb7492aab8d7a9a30e1530daaffaf8964b
Luiz Alexandre <luiz.alexandre@live.com>
2017-11-10 00:26:37 -0200

    Documentação dos métodos de ClienteDAO #13

--- RH/src/rh/modelo/ClienteDAO.java
+++ RH/src/rh/modelo/ClienteDAO.java
@@ -16,12 +16,18 @@ import rh.modelo.ClienteEnderecoDAO;
 
 /**
  * Realiza as resposabilidades comportamentais necessárias para a persistencia
- * de um funcionário no banco de dados
+ * de um Cliente no banco de dados
  *
- * @author L
  */
 public class ClienteDAO {
 
+	
+	/**
+	 * Método responsável por persistir um objeto da classe Cliente no banco de dados
+	 *
+	 * @param c Objeto da classe Cliente que será persistido no banco de dados
+	 * @return Retorna a chave primária do cliente persistido no banco de dados
+	 */
     public static int create(Cliente c) throws SQLException {
         Connection conn = BancoDados.createConnection();
 
@@ -51,6 +57,12 @@ public class ClienteDAO {
         return pkc;
     }
 
+    /**
+	 * Método responsável recuperar um usuário do banco de dados passando seu id
+	 *
+	 * @param pk_cliente id de cliente que será buscado no banco de dados
+	 * @return retorna o Cliente pesquisado
+	 */
     public static Cliente retrieve(int pk_cliente) throws SQLException {
         Connection conn = BancoDados.createConnection();
 
@@ -74,6 +86,12 @@ public class ClienteDAO {
         return c;
     }
 
+    
+    /**
+	 * Método responsável recuperar todos os Clientes do banco de dados
+	 *
+	 * @return Retorna todos os clientes salvos no banco
+	 */
     public static ArrayList<Cliente> retrieveAll() throws SQLException {
         Connection conn = BancoDados.createConnection();
 
@@ -99,6 +117,11 @@ public class ClienteDAO {
         return aux;
     }
     
+    /**
+	 * Método responsável por atualizar um registro de Cliente no banco
+	 *
+	 * @param c Objeto da classe Cliente que será atualizado
+	 */
     public static void update(Cliente c) throws SQLException{
         Connection conn = BancoDados.createConnection();
         
@@ -118,6 +141,11 @@ public class ClienteDAO {
         }
     }
     
+    /**
+	 * Método responsável por deletar um cliente do banco de dados
+	 *
+	 * @param c Objeto da classe Cliente que será apagado do banco de dados
+	 */
     public static void delete(Cliente c) throws SQLException{
         if (c.getPk()==0){
             throw new SQLException("Objeto não persistido ainda ou com a chave primária não configurada");
@@ -133,6 +161,11 @@ public class ClienteDAO {
         stm.close();        
     }
     
+    /**
+	 * Método responsável por deletar um cliente do banco de dados passando seu ID
+	 *
+	 * @param id Id do cliente que será apagado do banco de dados
+	 */
     public static void delete(int id) throws SQLException{
         if (id==0){
             throw new SQLException("Objeto não persistido ainda ou com a chave primária não configurada");
@@ -148,6 +181,12 @@ public class ClienteDAO {
         stm.close();        
     }
     
+    /**
+	 * Método responsável por returnar todos os clientes de uma determinada cidade
+	 *
+	 * @param nomeCidade Nome da cidade para fazer o filtro de cliente
+	 * @return Retorna todos os clientes de uma determinada cidade
+	 */
     public static ArrayList<Cliente> retrieveByCidade(String  nomeCidade) throws SQLException {
         
         ArrayList<Cliente> aux = new ArrayList<>();        
@@ -169,6 +208,12 @@ public class ClienteDAO {
          return aux;
     }
     
+    /**
+	 * Método responsável por retornar todos os clientes de um determinado Estado
+	 *
+	 * @param nomeEstado Nome do estado que será pesquisado
+	 * @return Todos os clientes de um determinado estado
+	 */
     public static ArrayList<Cliente> retrieveByEstado(String  nomeEstado) throws SQLException {
         
         ArrayList<Cliente> aux = new ArrayList<>();        
